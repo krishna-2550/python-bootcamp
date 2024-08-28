5476ffc0989024b2971d7ac3aaba39bc4ae286a6
Krishna Dandekar <KRISHNADANDEKAR2550@GMAIL.COM>
2024-08-28 17:43:30 +0530

    list

---
+++ lists.py
@@ -0,0 +1,23 @@
+# list = [1,2,3,4,"hello",'g',10.50]
+# print(list[6])
+
+#list methods
+
+list = [1,5,3,8,2,9]
+# list.sort()
+# print(list)
+
+# list.reverse()
+# print(list)
+
+# list.append(10)
+# print(list)
+
+# list.insert(2,10)
+# print(list)
+
+# list.pop(2)
+# print(list)
+
+# list.remove(5)
+# print(list)
