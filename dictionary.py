5fc2b034839e5594fa646069298592d4ae14ff68
Krishna Dandekar <KRISHNADANDEKAR2550@GMAIL.COM>
2024-09-07 17:50:28 +0530

    set and dict

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+# dict = {"name":"hello","age":20,"gender":"male"}
+# print(dict)
+# print(dict["name"])
+# print(dict["age"])
+# print(dict["gender"])
+
+# dict metods
+# print(dict.items())
+# print(dict.keys())
+# dict.update({"roll no":21})
+# print(dict)
+# print(dict.get("name"))
---
+++ set.py
@@ -0,0 +1,13 @@
+set = {1,2,3,4,5}
+set2 = {1,2,3,4,5,7,9}
+# print(len(set))
+# set.remove(5)
+# print(set)
+# set.pop()
+# print(set)
+# set.clear()
+# print(set.union())
+# set.intersection()
+# print(set)
+
+# print(type(set))
