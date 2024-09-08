b477315eb93df1eb22762736ac83934d45ca058a
sneha591 <s7961827@gmail.com>
2024-09-08 21:06:24 +0530

    Update list_tuple_precticeset.py

---
+++ list_tuple_precticeset.py
@@ -0,0 +1,14 @@
+#fruits = []
+#fruits = input("Enter fruits: ")
+#print(fruits)
+
+#a = (7,0,8,0,0,9,1)
+#print(a.count(0))
+
+#students = [95,33,72,100,10,55,]
+#students.sort()
+#print(students)
+
+sum = [10,20,30,40]
+sum = sum[0]+sum[1]+sum[2]+sum[3]
+
