e5dbe4fd0eb1cafea149d5e7ac391ba056dc6b0f
sneha591 <s7961827@gmail.com>
2024-09-14 17:58:42 +0530

    Update set_dict_precticeset.py

---
+++ set_dict_precticeset.py
@@ -0,0 +1,34 @@
+#dict = {"Namasta" : "Hello", "mam" : "mother", "pita" : "father", "bhai" : "brother"}
+#a = input("Enter keys ")
+#dict.get("a")
+#print(dict)
+
+#num = {}
+#num = (input("Enter num "))
+#print(num)
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add("20")
+len(s)
+
+print(len(s))
+
+s ={"key":"values"}
+s =(type(s))
+print(s)
+
+#dict = {}
+#keys = input("Enter names ")
+#values = input("Enter language")
+#print(keys,values)
+#dict.union({keys,values})
+
+#s = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,"a","b","c","d","e","f","g","h","k","i","j","l","m","n","o","p","q","r"}
+#print(s)
+
+
+s = {8,7,12,"Harry",[1,2]}
+s.pop(8)
+
